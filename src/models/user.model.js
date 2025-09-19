112966da3af6525420ae5f7077b8182999e39cfe
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-09-19 15:26:18 -0300

    Se crearon los models

---
+++ src/models/user.model.js
@@ -0,0 +1,43 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 20,
+      match: /^[a-zA-Z0-9_]+$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: /.+@.+\..+/,
+    },
+    password: { type: String, required: true, minlength: 6, select: false },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    profile: {
+      firs_name: { type: String, required: true, minlength: 2, maxlength: 50 },
+      last_name: { type: String, required: true, minlength: 2, maxlength: 30 },
+      biography: { type: String, maxlength: 500 },
+      avatar_url: { type: String },
+      birth_date: { type: Date },
+    },
+    deleted_at: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const UserModel = model("User", UserSchema);
