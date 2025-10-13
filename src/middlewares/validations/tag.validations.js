f3578b7eb583265856ecca536f2bd6699014e3df
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-10-12 21:16:10 -0300

    Se corrigio un error de importacion

--- src/middlewares/validations/tag.validations.js
+++ src/middlewares/validations/tag.validations.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { TagModel } from "../../models/tag.model.js";
 
 export const idTagValidations = [
